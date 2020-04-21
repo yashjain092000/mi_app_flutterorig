e66d8aacbf9b4f73e41cdcc309417f992faa89c1
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-04-21 02:01:14 -0500

    insert multiple containers in column method

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,38 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:
-    Scaffold(backgroundColor: Colors.teal,
-        body:SafeArea(child:  Container(color: Colors.white,
-            height:100.0,
-            width:100.0,
-            margin: EdgeInsets.only(left: 50,top:20),
-            padding: EdgeInsets.all(25)
-            ,child: Text("Container 1")
-        ))
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                height:100.0,
+                width:100.0,
+                //margin: EdgeInsets.only(left: 50,top:20),
+                padding: EdgeInsets.only(top:40,left:140),
+                child: Text("Container 1"),
+              ),
+              Container(color:Colors.red,
+                height:100.0,
+                width:100.0,
+                child: Text("Container 2"),
+              ),
+              Container(color:Colors.blue,
+                height:100.0,
+                width:100.0,
+                child: Text("Container 2"),
+              ),
 
-    )
+            ],
+          )
+
+        ),
+
+      ),
     );
   }
 }
