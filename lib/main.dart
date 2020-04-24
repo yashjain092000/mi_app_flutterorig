9be41e7db7a9fefa16a3148cc24099f4f1347bbf
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-04-24 17:55:47 -0500

    add another container including icons for email

--- lib/main.dart
+++ lib/main.dart
@@ -36,9 +36,12 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5),
             ),
             Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              padding: EdgeInsets.all(10),
               child: Row(children: <Widget>[
-                Icon(Icons.phone, color: Colors.white, size: 15),
-                SizedBox(width:10),
+                Icon(Icons.phone, color: Colors.teal, size: 18),
+                SizedBox(width: 10),
                 Text(
                   "+91 9634777609",
                   style: TextStyle(
@@ -50,6 +53,26 @@ class MyApp extends StatelessWidget {
                 )
               ]),
             ),
+            Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    size: 18,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(width:10),
+                  Text("yashjain092000@gmail.com",
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                      ),
+                  )
+                ]))
           ],
         )),
       ),
