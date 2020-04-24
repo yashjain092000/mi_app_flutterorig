9af84c5e622583fd4449eab237cb743fb5e95507
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-04-24 17:20:12 -0500

    add custom fonts and a caption under my name

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,28 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/yash.png'),
-                radius:52.0,
-              
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/yash.png'),
+                  radius: 52.0,
+                )
+                ,
+                Text("Yash Jain", style: TextStyle(color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+                ),
+                Text("FLUTTER DEVELOPER", style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                letterSpacing:2.5),
+                )
 
-
-              )
-              ,Text("Yash Jain",style: TextStyle(color:Colors.white,fontSize: 40,fontWeight: FontWeight.bold))
-
-            ],
-          )
+              ],
+            )
 
         ),
 
