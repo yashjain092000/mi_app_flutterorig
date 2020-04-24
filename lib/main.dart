aa449a64d46ed68c6af8a50b4feb71ce47d424c7
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-04-24 17:37:50 -0500

    add a icon in a row container and add phone number

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,46 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage('images/yash.png'),
-                  radius: 52.0,
-                )
-                ,
-                Text("Yash Jain", style: TextStyle(color: Colors.white,
-                    fontFamily: 'Pacifico',
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
-                ),
-                Text("FLUTTER DEVELOPER", style: TextStyle(
-                    color: Colors.teal.shade100,
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 22.0,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage('images/yash.png'),
+              radius: 52.0,
+            ),
+            Text(
+              "Yash Jain",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5),
+            ),
+            Container(
+              child: Row(children: <Widget>[
+                Icon(Icons.phone, color: Colors.white, size: 15),
+                SizedBox(width:10),
+                Text(
+                  "+91 9634777609",
+                  style: TextStyle(
+                    fontSize: 20,
                     fontWeight: FontWeight.bold,
-                letterSpacing:2.5),
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade900,
+                  ),
                 )
-
-              ],
-            )
-
-        ),
-
+              ]),
+            ),
+          ],
+        )),
       ),
     );
   }
 }
-
-
-
